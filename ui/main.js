380f49afbd79b980384b63c6964bf989983e56c5
Anonymous6767 <pramit.11.10@gmail.com>
2016-09-28 21:06:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,9 @@
 console.log('Loaded!');
+var marginleft=0;
+var moveright=function(){
+    marginleft=marginleft+1;
+    img.style.marginleft=marginleft+"px";
+};
+imag.onclick=function(){
+    var interval=setInterval(moveright,5);
+};
