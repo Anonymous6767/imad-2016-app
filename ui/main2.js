4e483d639256bf06cd3e54d7894242775978292d
Anonymous6767 <pramit.11.10@gmail.com>
2016-09-29 00:24:53 +0530

    [imad-console] Updates ui/main2.js

--- ui/main2.js
+++ ui/main2.js
@@ -1,1 +1,8 @@
-
+var buttn=document.getElementById("counter");
+var counter=0;
+counter.onclick=function(){
+  //rendering 
+  counter=counter+1;
+  var span=doocument.getElementById("count");
+  span.innerHTML=counter.toString();
+};
