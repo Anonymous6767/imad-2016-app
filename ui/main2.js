5b1c73a9b3541c45b3f68063f99f808a669c2f14
Anonymous6767 <pramit.11.10@gmail.com>
2016-09-29 00:46:34 +0530

    [imad-console] Updates ui/main2.js

--- ui/main2.js
+++ ui/main2.js
@@ -1,8 +1,25 @@
 var buttn=document.getElementById("counter");
-var counter=0;
+//var counter=0;
 counter.onclick=function(){
+    //create request
+    var request=new XMLHTTPRequest();
+    request.onreadystatechange=function(){
+        if(request.readyState===XMLHTTPRequest.DONE){
+            if(request.status===200){
+                var counter=request.responseText;
+                 var span=doocument.getElementById("count");
+                 span.innerHTML=counter.toString();
+                
+            }
+        }
+        
+    };
+    //work 
+    
   //rendering 
-  counter=counter+1;
-  var span=doocument.getElementById("count");
-  span.innerHTML=counter.toString();
+ // counter=counter+1;
+ //Make the request
+ request.open("GET","http://anonymous6767.imad.hasura-app.io/",true);
+ request.send(null);
+ 
 };
