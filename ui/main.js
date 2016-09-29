4dd303fa3e57f32766a7c3c815403efa4075933e
Anonymous6767 <pramit.11.10@gmail.com>
2016-09-29 13:31:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,3 +8,17 @@ var img=document.getElementById("pramit");
 img.onclick=function(){
     var interval=setInterval(moveright,5);
 };
+var nameInput=document.getElementByid("name");
+var name=nameInput.value;
+var submit=document.getElementById("submit_btn");
+submit.onclick=function(){
+    //rendering request
+    var names=["name1","name2","name3"];
+     var list="";
+     for(var i=0;i<names.length;i++){
+         list=='<li>'+names[i]+'</li>';
+         
+     }
+     var ul=documen.getElementById("namelist");
+     ul.innerHTML=list;
+}
