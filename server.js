fb3473d517d97a017950e45f447b5476f527ce7d
Anonymous6767 <pramit.11.10@gmail.com>
2016-10-28 15:29:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -44,6 +44,15 @@ var pool=new Pool(config);
 app.get('/test-db',function(req,res){
     //select
     //make a response
+    pool.query("SELECT * FROM test",function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+           res.send(JSON.stringify(result));  
+        }
+    })
+    
 })
 
 app.get('/ui/style.css', function (req, res) {
