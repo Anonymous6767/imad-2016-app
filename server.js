eb0a31521ca1497881b5bf37709d1ad390d9a1b2
Anonymous6767 <pramit.11.10@gmail.com>
2016-11-01 00:39:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -120,14 +120,14 @@ app.get('/aticles/:aticleName',function(req,res){
            res.status(500).send(err.toString());
        } 
        else{
-           if(result.rows.length===0){
-               res.status(404).send('Article Not Found');
+          // if(result.rows.length===0){
+           //    res.status(404).send('Article Not Found');
                
-           }
-           else{
+          // }
+          // else{
                var articleData=result.rows[0];
                res.send(createTemplate(articleData));
-           }
+          // }
        }
     });
     
