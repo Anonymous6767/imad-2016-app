7ff6d3b9531fcfa14565cb9c9e2bc15cf00c7465
Anonymous6767 <pramit.11.10@gmail.com>
2016-11-01 00:56:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -111,7 +111,7 @@ app.get('/test-db',function(req,res){
 var counter=0;
 app.get('/counter',function(req,res){
     counter=counter+1;
-    res.send(counter.toString(""));
+    res.send(counter.toString());
 });
 
 
