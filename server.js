3a422ad4f1a2c2c28735e6b492edb28dd965b2c7
Anonymous6767 <pramit.11.10@gmail.com>
2016-10-31 20:36:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/aticle-one',function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'aticle-one.html'));
+    res.send(createTemplate(articleone));
 });
 app.get('/aticle-two',function(req,res){
      res.sendFile(path.join(__dirname, 'ui', 'aticle-two.html'));
