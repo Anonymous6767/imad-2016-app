ae2fadd817d7856c5c0c5e7469b31b98f39b0cbd
Anonymous6767 <pramit.11.10@gmail.com>
2016-11-03 13:01:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -138,8 +138,8 @@ app.get('/aticles/:aticleName',function(req,res){
     
 });
 
-
-app.get('/submit-name/name', function (req, res) {
+var names=[];
+app.get('/submit-name/:name', function (req, res) {
     var names=req.params.name;
     
     
@@ -160,7 +160,7 @@ app.get('/ui/madi.png', function (req, res) {
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
-var names=[];
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
