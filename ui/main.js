b9ef64e98a548a525d913619dc5770f53a49876e
Anonymous6767 <pramit.11.10@gmail.com>
2016-11-03 15:56:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,12 +25,24 @@ var nameInput=document.getElementById('name');
 var name1=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
-  var names=['name1','name2','name3'];
-  var list='';
-  for(var i=0;i<names.length;i++){
+     var request=new XMLHttpRequest();
+       request.onreadystatechange=function(){
+        if (request.readyState===XMLHttpRequest.DONE){
+            if(request.status===200){
+    
+                var names=['name1','name2','name3'];
+                var list='';
+                 for(var i=0;i<names.length;i++){
       list+='<li>'+names[i]+'</li>';
   }
+            
+        
   var ul=document.getElementById('namelist');
   ul.innerHTML=list;
+            }
+        }
+       };
+         request.open('GET','http://anonymous6767.imad.hasura-app.io/counter',true); 
+    request.send(null);
 };
 
