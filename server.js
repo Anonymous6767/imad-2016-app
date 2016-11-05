fec2d0603d58c94670c4858f41a809f6c2767c65
Anonymous6767 <pramit.11.10@gmail.com>
2016-11-05 16:58:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -159,12 +159,13 @@ app.get('/articles/:articleName',function(req,res){
   
     
 });
-function hash(input){
+function hash(input,salt){
     var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sho512');
+    return hashed;
 }
 
 app.get('/hash/:input',function(req,res){
-   var hashedString=hash(req.params.input);
+   var hashedString=hash(req.params.input,salt);
    res.send(hashedString);
 });
 
